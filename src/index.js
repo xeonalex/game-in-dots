a65ad1575ae9a4263f8d5d9f25826ba475a03945
Alex Saulyak <50foots@gmail.com>
2019-05-30 18:26:06 +0300

    folding refactoring

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from 'components/App/App';
+import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
-import configureStore from '_redux/store';
+import configureStore from './_redux/store';
 
 ReactDOM.render(
     <Provider  store={configureStore()}>
